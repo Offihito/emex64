9021b0e5b490992f3be6b20a1bb27a25ff01ea52
mach-port-t <fridakitten@pm.me>
2026-06-11 10:47:35 +0200

    perf: faster decoder in core

--- emex64lib/src/vm/core.c
+++ emex64lib/src/vm/core.c
@@ -153,14 +153,11 @@ static inline bool emex64_core_decode_instruction_at_pc(emex64_core_t *core)
 {
     uint64_t pc_addr = core->rl[kEmex64RegisterPC];
     uint64_t cr_pte = core->rl[kEmex64RegisterCR4];
-    if(((cr_pte & EMEX64_MMU_MASK_FLAGS) & kEmex64MMUPTPresent) && !core->in_interrupt)
+    if((((cr_pte & EMEX64_MMU_MASK_FLAGS) & kEmex64MMUPTPresent) && !core->in_interrupt) &&
+        unlikely(!emex64_mmu_access(core, pc_addr, kEmex64MMUAccessExec, &pc_addr)))
     {
-        /* incase paging is disabled */
-        if(unlikely(!emex64_mmu_access(core, pc_addr, kEmex64MMUAccessExec, &pc_addr)))
-        {
-            /* MMU wrote exception, not needed to fill in our own */
-            return false;
-        }
+        /* MMU wrote exception, not needed to fill in our own */
+        return false;
     }
 
     /*
