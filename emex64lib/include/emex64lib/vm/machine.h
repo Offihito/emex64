8023a37639c0d887b5a6657cb9be90e334d386dd
mach-port-t <fridakitten@pm.me>
2026-06-13 10:53:03 +0200

    fix: correcting bounds checks in machine

--- emex64lib/include/emex64lib/vm/machine.h
+++ emex64lib/include/emex64lib/vm/machine.h
@@ -60,10 +60,10 @@ emex64_machine_options_t emex64_machine_options_default(void);
 
 static inline bool emex64_memory_access(emex64_core_t *core,
                                         uint64_t addr,
-                                        size_t size)
+                                        uint64_t size)
 {
     uint64_t addr_end = addr + size;
-    if(addr >= addr_end || core->machine->memory->memory_size < addr_end)
+    if(addr > addr_end || core->machine->memory->memory_size < addr_end)
     {
         return false;
     }
